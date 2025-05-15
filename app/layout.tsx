1813f63eda5c8eda7417d2bcd2422ed845eb8421
Platon Kolesnikov <157084702+puponkoko@users.noreply.github.com>
2025-05-15 11:23:18 -0700

    meta tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,10 +30,12 @@ export default function RootLayout({
     return (
         <html lang="en">
         <Head>
+            <meta name="google-site-verification" content="Z1MOzUsYOpdH3tPltzgnQgnAl4ItmWFWLBPx3nkFNIA"/>
+
             {/* Favicons */}
-            <link rel="icon" href="/favicon.ico" />
-            <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-            <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+            <link rel="icon" href="/favicon.ico"/>
+            <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+            <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
 
             {/* Structured Data for SEO */}
             <script
